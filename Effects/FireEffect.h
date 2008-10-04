bdf55d22925cfa0024b1947a02da6913fd631d77
salomonhippie <salomonhippie@gmail.com>
2008-10-04 11:34:14 +0000

    emit point based on transformationNode hierarchy

--- Effects/FireEffect.h
+++ Effects/FireEffect.h
@@ -51,8 +51,91 @@ using namespace Math;
 
 typedef Color < Texture <Size < PreviousPosition < Position < Life < IParticle > > > > > >  TYPE;
 
-class FireEffect : public IRenderNode, public IParticleEffect {
+class FireEffect : public IParticleEffect, public TransformationNode {
 private:
+    class ParticleRenderer: public IRenderNode {
+    public:
+        ParticleRenderer(ParticleCollection<TYPE>* particles):
+            particles(particles) {}
+        virtual ~ParticleRenderer() {}
+
+        void Apply(IRenderingView* view) {
+        
+            // @todo: we need to move all this gl specific code into the renderer
+            // @todo: also glpushmatrix is too expensive. let's make our own calculations 
+            //        of the rotation and position of the quad.
+            
+            glPushAttrib(GL_LIGHTING);    
+            glDisable(GL_LIGHTING);
+            glDepthMask(GL_FALSE);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+            glEnable(GL_TEXTURE_2D);
+            glEnable(GL_COLOR_MATERIAL);
+    
+            for (particles->iterator.Reset(); particles->iterator.HasNext(); particles->iterator.Next()) {
+                TYPE& particle = particles->iterator.Element();
+                ITextureResourcePtr texr = particle.texture;
+            
+                //Set texture
+                if (texr != NULL) {
+                    if (texr->GetID() == 0) {
+                        TextureLoader::LoadTextureResource(texr);
+                        //logger.info << texr->GetID() << logger.end;
+                    }
+                    glBindTexture(GL_TEXTURE_2D, texr->GetID());
+                }
+            
+                else {
+                    glBindTexture(GL_TEXTURE_2D, 0);
+                }
+            
+                glPushMatrix();
+                glTranslatef(particle.position[0], particle.position[1], particle.position[2]);
+            
+                // billboard
+                float modelview[16];
+                glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
+                modelview[0] = modelview[5] = modelview[10] = 1.0;
+                modelview[1] = modelview[2] = modelview[4] = 
+                    modelview[6] = modelview[8] = modelview[9] = 0.0; 
+                glLoadMatrixf(modelview);
+
+                // apply quad transformations
+                glRotatef(particle.rotation, 0,0,1);
+                float scale = particle.size;
+                glScalef(scale,scale,scale);
+                float c[4];
+                particle.color.ToArray(c);
+                glColor4fv(c);
+            
+                glBegin(GL_QUADS);
+                glTexCoord2f(0.0, 0.0);
+                glVertex3f(-1.0, -1.0, 0.0);
+                glTexCoord2f(0.0, 1.0);
+                glVertex3f(-1.0, 1.0, 0);
+                glTexCoord2f(1.0, 1.0);
+                glVertex3f(1, 1, 0);
+                glTexCoord2f(1.0, 0.0);
+                glVertex3f(1, -1, 0);
+                glEnd();
+            
+                glPopMatrix();
+            }
+        
+            glDisable(GL_BLEND);
+            glPopAttrib();
+        
+            // render subnodes
+            VisitSubNodes(*view);      
+        }
+
+    private:
+        ParticleCollection<TYPE>* particles;
+    };
+    
+    ParticleRenderer* pr;
+    
     ParticleCollection<TYPE>* particles;
 
     OpenEngine::ParticleSystem::ParticleSystem* system;
@@ -69,14 +152,14 @@ private:
     TextureRotationModifier<TYPE> rotationmod;
 
     RandomGenerator randomgen;
-
     
+
 public:
     FireEffect(OpenEngine::ParticleSystem::ParticleSystem* system): 
         system(system),
         wind(Vector<3,float>(1.591,0,0)),
         antigravity(Vector<3,float>(0,0.382,0)),
-        sizemod(20.0){
+        sizemod(2.0){
         particles = system->CreateParticles<TYPE>(500);     
         
         //load texture resource
@@ -89,6 +172,8 @@ public:
         inittex.AddTextureResource(texr3);
         
         randomgen.SeedWithTime();
+        
+        pr = new ParticleRenderer(particles);
 }
 
 ~FireEffect() {
@@ -131,17 +216,19 @@ void inline Emit() {
     static const float numberVar = 2;
     
     // attributes for emission on square
-    static const Vector<3,float> position(0.0, -30.0, -50.0);
-    static const Vector<3,float> devAxis1(20.0,0.0,0.0);
-    static const Vector<3,float> devAxis2(0.0,0.0,20.0);        
+    Vector<3,float> position;
+    Quaternion<float> direction;
+    GetAccumulatedTransformations(&position, &direction);
+
+    //static const Vector<3,float> position(0.0, -30.0, -50.0);
+    //static const Vector<3,float> devAxis1(20.0,0.0,0.0);
+    //static const Vector<3,float> devAxis2(0.0,0.0,20.0);        
     
     static const float life = 2100;
     static const float lifeVar = 1000;
     
-    static const float size = 7;
-    static const float sizeVar = 2;
-    
-    static const Vector<3,float> velocity(0.0,2.0,0.0);
+    static const float size = 1;
+    static const float sizeVar = 0;
     
     // angle is the angular deviation from the direction of
     // the velocity
@@ -159,8 +246,9 @@ void inline Emit() {
         TYPE& particle = particles->NewParticle();
         
         // static pos
-        particle.position = position + devAxis1*randomgen.UniformFloat(-1.0,1.0) 
-            + devAxis2*randomgen.UniformFloat(-1.0,1.0);
+        particle.position = position;
+        //+ devAxis1*randomgen.UniformFloat(-1.0,1.0) 
+        //    + devAxis2*randomgen.UniformFloat(-1.0,1.0);
             
         particle.life = 0;
         particle.maxlife = RandomAttribute(life, lifeVar);
@@ -176,91 +264,21 @@ void inline Emit() {
         inittex.Process(particle);
     
         // random direction
-        float r = (rand()/((float)RAND_MAX))*RandomAttribute(angle, angleVar);
-        float p = (rand()/((float)RAND_MAX))*2*PI;
-        Quaternion<float> q(r,p, 0);
-        q.Normalize();
+//         float r = (rand()/((float)RAND_MAX))*RandomAttribute(angle, angleVar);
+//         float p = (rand()/((float)RAND_MAX))*2*PI;
+//         Quaternion<float> q(r,p, 0);
+//         q.Normalize();
     
         // set the previous position
         // this will represent direction and speed when using verlet 
         // integration for updating position
-        particle.previousPosition = particle.position - (q.RotateVector(velocity));
+        particle.previousPosition = particle.position - 
+            (direction.RotateVector(Vector<3,float>(0.0,-1.0,0.0)));
     }
-}
-
-void Apply(IRenderingView* view) {
-        
-    // @todo: we need to move all this gl specific code into the renderer
-    // @todo: also glpushmatrix is too expensive. let's make our own calculations 
-    //        of the rotation and position of the quad.
-
-    glPushAttrib(GL_LIGHTING);    
-    glDisable(GL_LIGHTING);
-    glDepthMask(GL_FALSE);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_TEXTURE_2D);
-    glEnable(GL_COLOR_MATERIAL);
-    
-    for (particles->iterator.Reset(); particles->iterator.HasNext(); particles->iterator.Next()) {
-        TYPE& particle = particles->iterator.Element();
-        ITextureResourcePtr texr = particle.texture;
-            
-        //Set texture
-        if (texr != NULL) {
-            if (texr->GetID() == 0) {
-                TextureLoader::LoadTextureResource(texr);
-                //logger.info << texr->GetID() << logger.end;
-            }
-            glBindTexture(GL_TEXTURE_2D, texr->GetID());
-        }
-            
-        else {
-            glBindTexture(GL_TEXTURE_2D, 0);
-        }
-            
-        glPushMatrix();
-        glTranslatef(particle.position[0], particle.position[1], particle.position[2]);
-            
-        // billboard
-        float modelview[16];
-        glGetFloatv(GL_MODELVIEW_MATRIX, modelview);
-        modelview[0] = modelview[5] = modelview[10] = 1.0;
-        modelview[1] = modelview[2] = modelview[4] = 
-            modelview[6] = modelview[8] = modelview[9] = 0.0; 
-        glLoadMatrixf(modelview);
-
-        // apply quad transformations
-        glRotatef(particle.rotation, 0,0,1);
-        float scale = particle.size;
-        glScalef(scale,scale,scale);
-        float c[4];
-        particle.color.ToArray(c);
-        glColor4fv(c);
-            
-        glBegin(GL_QUADS);
-        glTexCoord2f(0.0, 0.0);
-        glVertex3f(-1.0, -1.0, 0.0);
-        glTexCoord2f(0.0, 1.0);
-        glVertex3f(-1.0, 1.0, 0);
-        glTexCoord2f(1.0, 1.0);
-        glVertex3f(1, 1, 0);
-        glTexCoord2f(1.0, 0.0);
-        glVertex3f(1, -1, 0);
-        glEnd();
-            
-        glPopMatrix();
-    }
-        
-    glDisable(GL_BLEND);
-    glPopAttrib();
-        
-    // render subnodes
-    VisitSubNodes(*view);      
 }
 
 ISceneNode* GetSceneNode() {
-    return this;
+    return pr;
 }
 
 };
